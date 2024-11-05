b1bf17b2249512742269ce3d97c396e89cf36f49
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2024-11-05 20:26:54 +0800

    update connection and header

--- components/header.php
+++ components/header.php
@@ -3,30 +3,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>RJM &mdash; Transport Corporation</title>
-    <meta name="description" content="Free Bootstrap 4 Theme by ProBootstrap.com">
-    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
-    
+    <meta name="description" content="An Online Booking and Scheduling System">
+    <meta name="keywords" content="bus scheduling and booking system.">
+
 
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,700" rel="stylesheet">
 
-	<link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet" href="assets/fonts/ionicons/css/ionicons.min.css">
-    
+
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    
+
     <link rel="stylesheet" href="assets/fonts/flaticon/font/flaticon.css">
 
     <link rel="stylesheet" href="assets/fonts/fontawesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/select2.css">
-    
+
 
     <link rel="stylesheet" href="assets/css/helpers.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/booking-style.css">
 
     <script src="assets/js/jquery.min.js"></script>
-    
+
     <script src="assets/js/popper.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/owl.carousel.min.js"></script>
@@ -40,4 +40,3 @@
 
     <script src="assets/js/booking.js"></script>
 </head>
-
